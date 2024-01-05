f2d0d858a45ef05a0ee45a6698abcb62a1255124
Shivk123 <shivkhetan18@gmail.com>
2024-01-05 21:39:45 +0530

    Random Order Added

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ var timetableData = {
     CNS: { turns: 3, connected: false, isLab: false, isDone: false },
     WAD: { turns: 3, connected: false, isLab: false, isDone: false },
     DSBDA: { turns: 3, connected: false, isLab: false, isDone: false },
-    HON: { turns: 4, connected: true, isLab: false, isDone: false },
     Internship: { turns: 4, connected: true, isLab: false, isDone: false },
+    HON: { turns: 4, connected: true, isLab: false, isDone: false },
     AUDIT_Course: { turns: 2, connected: true, isLab: false, isDone: false },
     PDA: { turns: 2, connected: true, isLab: false, isDone: false },
     CC_Activities: { turns: 2, connected: true, isLab: false, isDone: false },
-    
+
 };
 
 var timetableMatrix = [];
@@ -37,7 +37,7 @@ function showPanel(panelIndex) {
 
 function generateTimetable() {
     var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    
+
 
     // Initialization
     for (var i = 0; i < 7; i++) {
@@ -52,14 +52,18 @@ function generateTimetable() {
     console.log(timetableMatrix);
 
     // Filling the timetable matrix
-    
-    for(var i =0;i<7;i++){
+    let days = [0, 3, 2, 1, 4, 5, 6];
+    for (var i in days) {
 
         var labdone = 0;
+        i = days[i];
+        var c = i % 3;
 
-        for(var j =0;j<6;j++){
-            
-            for(var k in timetableData){
+        for (var j = 0; j < 6; j++) {
+            var a = j;
+            j = (j + 2 * c) % 6;
+
+            for (var k in timetableData) {
                 var subject = k;
                 console.log(subject);
                 var subjectTurns = timetableData[subject].turns;
@@ -67,44 +71,46 @@ function generateTimetable() {
                 var labb = timetableData[subject].isLab;
                 var donee = timetableData[subject].isDone;
                 console.log(subjectTurns, connected);
-                if(donee){
+                if (donee) {
                     continue;
                 }
-                if(labb && labdone){
+                if (labb && labdone) {
                     continue;
                 }
-                if(subjectTurns==0){
+                if (subjectTurns == 0) {
                     continue;
                 }
-                if(labb && subjectTurns>0){
+                if (labb && subjectTurns > 0) {
                     timetableMatrix[j][i] = { subject };
-                    timetableMatrix[j+1][i] = { subject };
+                    timetableMatrix[j + 1][i] = { subject };
                     j++;
-                    timetableData[subject].turns -=2;
-                    labdone=1;
+                    a++;
+                    timetableData[subject].turns -= 2;
+                    labdone = 1;
                     timetableData[subject].isDone = true;
                     break;
                 }
-                if(connected && (j&1)){
+                if (connected && (j & 1)) {
                     continue;
                 }
-                else if(connected){
+                else if (connected) {
                     timetableMatrix[j][i] = { subject };
-                    timetableMatrix[j+1][i] = { subject };
+                    timetableMatrix[j + 1][i] = { subject };
                     j++;
-                    timetableData[subject].turns -=2;
+                    timetableData[subject].turns -= 2;
                     timetableData[subject].isDone = true;
                     break;
                 }
-                else{
+                else {
                     timetableMatrix[j][i] = { subject };
-                    timetableData[subject].turns -=1;
+                    timetableData[subject].turns -= 1;
                     timetableData[subject].isDone = true;
                     break;
                 }
             }
+            j = a;
         }
-        for(var k in timetableData){
+        for (var k in timetableData) {
             var subject = k;
             timetableData[subject].isDone = false;
 
@@ -119,7 +125,7 @@ function generateTimetable() {
             var cell = document.getElementById(cellId);
             var cellContent = timetableMatrix[i][j] ? timetableMatrix[i][j].subject : '';
             var isLab = timetableMatrix[i][j] && timetableMatrix[i][j].lab;
-    
+
             if (cell) {
                 cell.textContent = cellContent;
             } else {
@@ -127,16 +133,16 @@ function generateTimetable() {
             }
         }
     }
-    
+
 }
 
-function resetTimetable(){
+function resetTimetable() {
     timetableData = original_timetable;
     for (var i = 0; i < 6; i++) {
         for (var j = 0; j < 6; j++) {
             var cellId = 'slot' + (i + 1) + '-' + (j + 1);
             var cell = document.getElementById(cellId);
-    
+
             if (cell) {
                 cell.textContent = '';
             } else {
